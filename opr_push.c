fc95c6cef22f04f850d079cb5ebbade88d719496
Ana Luiza Fretta Barros <analufretta@gmail.com>
2025-12-23 17:38:39 +0100

    functions: swap, swap_a, swap_b swap_ss

--- opr_push.c
+++ opr_push.c
@@ -3,26 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   opr_push.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afretta- <afretta-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: afretta- <afretta-@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 18:34:02 by afretta-          #+#    #+#             */
-/*   Updated: 2025/12/18 18:38:32 by afretta-         ###   ########.fr       */
+/*   Updated: 2025/12/23 17:30:09 by afretta-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	push_a()
+void	push_a(t_stack_node **a, t_stack_node **b)
 {
 	if (!b)
-		do nothing
-		return (0);
+		return ;
 	else
 		take top b and put it ar top a
 	return(write(1, "pa\n", 3));
 }
 
-int	push_b()
+int	push_b(t_stack_node **a, t_stack_node **b)
 {
 	if (!a)
 		do nothing
--- opr_swap.c
+++ opr_swap.c
@@ -6,43 +6,46 @@
 /*   By: afretta- <afretta-@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 18:26:53 by afretta-          #+#    #+#             */
-/*   Updated: 2025/12/22 16:10:24 by afretta-         ###   ########.fr       */
+/*   Updated: 2025/12/23 17:36:50 by afretta-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	swap_elements(t_stack_node **stack)//WRONG! THINK ABOUT PREVIOUS NODES
+void	swap_elements(t_stack_node **stack)
 {
 	t_stack_node *first;
 	t_stack_node *second;
 
 	if (!stack || !(*stack) || !(*stack)->next)
 		return ;
-	else
-		first = (*stack)->next;
-		second = *stack;
-		*stack = first;
-		(*stack)->next = second;
 
-
+	first = (*stack);
+	second = first->next;
+	first->next = second->next;
+	if(second->next)
+		second->next->prev = first;
+	second->prev = NULL;
+	second->next = first;
+	first->prev = second;
+	*stack = second;
 }
 
-int	swap_a(t_stack_node **stack_a)
+void	swap_a(t_stack_node **stack_a)
 {
 	swap_elements(stack_a);
-	return(write(1, "sa\n", 3));
+	write(1, "sa\n", 3);
 }
 
-int	swap_b(t_stack_node **stack_b)
+void	swap_b(t_stack_node **stack_b)
 {
 	swap_elements(stack_b);
-	return(write(1, "sb\n", 3));
+	write(1, "sb\n", 3);
 }
 
-int	swap_ss(t_stack_node **stack_a, t_stack_node **stack_b)
+void	swap_ss(t_stack_node **stack_a, t_stack_node **stack_b)
 {
 	swap_elements(stack_a);
 	swap_elements(stack_b);
-	return(write(1, "sb\n", 3));
+	write(1, "sb\n", 3);
 }
